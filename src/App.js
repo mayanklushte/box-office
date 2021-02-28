1c85b68f3850be385036ea2a93bbfb1f12e0c3d9
mayanklushte <lushtemayank21@gmail.com>
2021-02-28 11:52:02 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import  { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div >
-      hi
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        This is Homepage
+      </Route>
+      <Route exact path='/stared'>
+        This is Stared Page
+      </Route>
+      <Route >
+        This Page Not Found or 404
+      </Route>
+    </Switch>
   );
 }
 
