5aa760837352cc8de6d5446766042c216a8455d3
mayanklushte <lushtemayank21@gmail.com>
2021-03-01 18:45:04 +0530

    created input and fetch function from tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 // children layout 
@@ -8,11 +8,37 @@ import MainPageLayout from '../components/MainPageLayout'
 // this how we should manage layouts in react
 
 const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    // this is browser's api fetch which fetch data from remote links or api
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result=>{
+      console.log(result);
+      
+    }); 
+    
+  };
+
+  const onInputChange = (ev) => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = (ev) =>{
+    if(ev.keyCode===13){
+      onSearch()
+    };
+  };
+
+// onkey down is function where we check which key is press using javascript keycode
+// on chnage will handle change of element and {input} is used to set useState element to input box
+
   return (
     <MainPageLayout>
-     This is Home
+     <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+     <button type='button' onClick={onSearch}>Search</button>
+
     </MainPageLayout>
-  )
-}
+  );
+};
 
 export default Home
