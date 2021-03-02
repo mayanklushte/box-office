3bf3514ca5100b6c02bce202c69115d101d008a2
mayanklushte <lushtemayank21@gmail.com>
2021-03-02 19:44:29 +0530

    replaced useState with useReducer on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,16 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 // useEffect is other custom hook which accepts callbackfunction and array of dpendencies
 // and when one of the dependency changes it call that function again
 // useParams is also a hook which gets variavles passed from url
+// here useState is used to manage states but later we changed it with useReduser
+// useReducer manage state to but of complex things like objects arrays and more..
+// it uses action, dispatch, reducer process
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoding, setIsLoding] = useState(true);
-  const [error, setError] = useState(null);
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  // const [show, setShow] = useState(null);
+  // const [isLoding, setIsLoding] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
@@ -18,14 +46,12 @@ const Show = () => {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoding(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoding(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
 
@@ -35,7 +61,7 @@ const Show = () => {
   }, [id]);
 
   console.log('show', show);
-  if (isLoding) {
+  if (isLoading) {
     return <div>data is still loading</div>;
   }
   if (error) {
