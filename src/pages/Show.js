489423a27154c68b25687e4d518d7f8bc23370ea
mayanklushte <lushtemayank21@gmail.com>
2021-03-02 20:29:12 +0530

    created show page components and populate them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 // useEffect is other custom hook which accepts callbackfunction and array of dpendencies
@@ -67,7 +72,35 @@ const Show = () => {
   if (error) {
     return <div>error ocurred: {error}</div>;
   }
-  return <div>this is Show page</div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
