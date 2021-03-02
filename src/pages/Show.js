5a9a9d14bcd7d97714699b3d6dcc3da11b1e28e6
mayanklushte <lushtemayank21@gmail.com>
2021-03-02 19:17:09 +0530

    added some comments

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+// useEffect is other custom hook which accepts callbackfunction and array of dpendencies
+// and when one of the dependency changes it call that function again
+// useParams is also a hook which gets variavles passed from url
+
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
