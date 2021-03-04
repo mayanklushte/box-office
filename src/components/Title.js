f2e24b67376721f62f1469162f2c068d425bc835
mayanklushte <lushtemayank21@gmail.com>
2021-03-04 12:12:13 +0530

    define theme for styled components and created few styled components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { TitleWrapper } from './Title.styled';
 
 // always pass pros like title and sub inside this  { }
 
 const Title = ({ title, subtitle }) => {
   return (
-    <div>
+    <TitleWrapper>
       <h1>{title}</h1>
       <p>{subtitle}</p>
-    </div>
+    </TitleWrapper>
   );
 };
 
